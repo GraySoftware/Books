1b55313d505c99c5f87e55df693870a3703e563d
GraySoftware <119638974+GraySoftware@users.noreply.github.com>
2023-01-02 18:16:09 -0500

    Added home details page

--- BooksWeb/Areas/Customer/Controllers/HomeController.cs
+++ BooksWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Books.Models;
+﻿using Books.DataAccess.Repository.IRepository;
+using Books.Models;
+using Books.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace BooksWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         // url: /home/index would return the contents of this routine
@@ -19,7 +23,19 @@ namespace BooksWeb.Controllers
         // so filepath views/home/index.cshtml
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(m => m.Id == id, includeProperties: "Category,CoverType")
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
