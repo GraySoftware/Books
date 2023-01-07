084212f2f4fc201a338d3e6a8100f78521a613f9
GraySoftware <119638974+GraySoftware@users.noreply.github.com>
2023-01-07 15:10:16 -0500

    sdfsdf

--- BooksWeb/Program.cs
+++ BooksWeb/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddRazorPages()
