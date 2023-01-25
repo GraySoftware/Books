88e2abd7879a9787a31afd41e9f734813b5e8a87
GraySoftware <119638974+GraySoftware@users.noreply.github.com>
2023-01-24 20:22:17 -0500

    Added order view model and api get all call

---
+++ BooksWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
