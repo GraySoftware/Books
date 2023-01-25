3b273aa1cc374a6461a7820b32444d22762cc943
GraySoftware <119638974+GraySoftware@users.noreply.github.com>
2023-01-24 20:38:35 -0500

    Added manage order to display all orders

--- BooksWeb/Areas/Admin/Controllers/OrderController.cs
+++ BooksWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Books.DataAccess.Repository.IRepository;
+using Books.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrderController : Controller
     {
+        private readonly IUnitOfWork _UnitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            orderHeaders = _UnitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
     }
 }
