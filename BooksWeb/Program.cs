c8b1e39344bc77530116abac117df4b6faee4dc5
GraySoftware <119638974+GraySoftware@users.noreply.github.com>
2023-02-07 21:23:09 -0500

    Added DB initializer

--- BooksWeb/Program.cs
+++ BooksWeb/Program.cs
@@ -7,7 +7,7 @@ using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Books.Utility;
 using Stripe;
-
+using Books.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 // if we want to register anything in our dependency injector do it here
@@ -23,6 +23,7 @@ builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Str
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddAuthentication().AddFacebook(options =>
@@ -61,6 +62,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("StripeSettings:SecretKey").Get<string>();
+SeedDatabase();
 app.UseAuthentication();
 
 app.UseAuthorization();
@@ -72,3 +74,12 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
